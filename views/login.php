4ac84c99329aea8e552e9c99cce9b31556e1f81a
Evellyn <flaviane.evellyn@alunoescolassociais.org.br>
2024-09-17 08:31:08 -0300

    Criamos uma tela de login  com php.

---
+++ views/login.php
@@ -0,0 +1,24 @@
+    <!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <title>Login</title>
+    </head>
+    <body>
+        <main>
+            <form action="">
+                <section>
+                    <label for="email">Email</label>
+                    <input type="email" name="email" placeholder= "email" required>
+</section>
+<section>
+    <label for="">Senha</label>
+    <input type="password" name="senha"placeholder="Senha" required>
+</section>
+<button type="submit">Login</button>
+</form>
+<a href="">Cadastre-se</a>
+</main>              
+    </body>
+    </html>
